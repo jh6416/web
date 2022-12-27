8588f6b2c65cf74380d777dbf4f4dbf887017358
gisuhub <121483326+gisuhub@users.noreply.github.com>
2022-12-27 19:29:43 +0900

    어아잉ㅇ
    ㅇㅁㄴㅇㅁㄴㅇㄴㅁ

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,15 @@
 function inform(){
     alert("나는 alert 입니다.");
 }
+
+function grade() {
+    var kor1 = document.getElementById(kor1).innerHTML;
+    var kor2 = document.getElementById(kor2).innerHTML;
+    document.getElementById("first_g").innerHTML=kor1+kor2;
+    
+    var eng1 = document.getElementById(eng1).innerHTML;
+    var eng2 = document.getElementById(eng2).innerHTML;
+    document.getElementById("second_g").innerHTML;
+    
+    document.getElementById("third_g").innerHTML="78";
+}
